0771d6c46c48ad1775a58d34c7b99a08a7b55167
Laras Rasdiyani <61336325+larasras@users.noreply.github.com>
2021-10-20 16:16:56 +0700

    init vue-project

---
+++ vue-project/src/components/Timer.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{id : string, status: string, elapsedTime: number, time: number}>()
+
+const formatted_time = ref("00:00:00")
+const result = ref("")
+const interval = ref(0)
+</script>
+
+<template>
+    <div>
+        <p>{{$props.id}}</p>
+
+        <p class="time">{{formatted_time}}</p>
+        <button>Pause</button>`
+        <button>Start/Resume</button>
+        <button>Stop</button>
+        <button>Reset</button>
+    </div>
+</template>
+
+<style scoped>
+.time {
+    font-size: 2em;
+}
+
+button {
+    width: 100px;
+    height: 40px;
+    background: transparent;
+    color: #384856;
+    border: 2px solid #384856;
+    border-radius: 5px;
+    outline: none;
+}
+
+button:hover {
+    background: transparent;
+    color: #b76b6d;
+    font-weight: 800;
+    text-transform: capitalize;
+}
+</style>
